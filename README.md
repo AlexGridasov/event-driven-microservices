14d21af39802cd34e48c764a05855f3162a2d79d
Alex Gridasov <alexgridasov@users.noreply.github.com>
2024-12-11 22:40:10 +0200

    twitter-to-kafka-service: Streaming tweets with Twitter4j [not working]
    The command component in CQRS & Event sourcing. v1 API endpoint disabled

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # event-driven-microservices
+
+# Running the application
+- Please enter the correct credentials in twitter4j.properties file.
+- Then run TwitterToKafkaServiceApplication inside IntelliJ, or run with mvn spring-boot:run command
--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,12 +1,14 @@
 package com.microservices.demo.twitter.to.kafka.service;
 
 import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import java.util.Arrays;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.microservices.demo")
@@ -14,10 +16,13 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
 
-    private final TwitterToKafkaServiceConfigData twitterToKafkaConfigData;
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+    private final StreamRunner streamRunner;
 
-    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData) {
-        this.twitterToKafkaConfigData = configData;
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData,
+                                            StreamRunner runner) {
+        this.twitterToKafkaServiceConfigData = configData;
+        this.streamRunner = runner;
     }
 
     public static void main(String[] args) {
@@ -25,9 +30,11 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) {
+    public void run(String... args) throws Exception {
         LOG.info("App starts...");
-        LOG.info(twitterToKafkaConfigData.getTwitterKeywords().toString());
-        LOG.info(twitterToKafkaConfigData.getWelcomeMessage());
+        LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
+        LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
+
+        streamRunner.start();
     }
 }
